d5ad0b358899ac7974e460227631746ceffffea3
gitSuki <collinbrown226@gmail.com>
2023-02-22 14:58:49 -0600

    Implement rabbitmq message publisher

--- broker-service/event/request.go
+++ broker-service/event/request.go
@@ -1,9 +1,12 @@
 package event
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/gitsuki/finance/broker/util"
+	"github.com/google/uuid"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -24,6 +27,15 @@ type consumeParams struct {
 	ShouldNoLocal bool       // no-local (whether or not messages published by this consumer should be ignored by itself)
 	ShouldNotWait bool       // nowait (client shouldn't wait for a response from the server before proceeding with the next operation)
 	Arguments     amqp.Table // arguments
+}
+
+type publishParams struct {
+	Context      context.Context
+	ExchangeName string // exchange (exchange to publish the message to)
+	RoutingKey   string // routing key (routing key for the message. used by the exchange to determine which queues the message should be delivered to)
+	IsMandatory  bool   // mandatory (whether the message must be routed to at least one queue. if true and the message cannot be routed, the message will be returned to the publisher)
+	IsImmediate  bool   // immediate (whether the message should be delivered immediately. if set to true and the message cannot be delivered to a consumer immediately, it will be returned to the publisher)
+	Message      amqp.Publishing
 }
 
 func SendRequest(config util.Config, message string) {
@@ -62,7 +74,7 @@ func SendRequest(config util.Config, message string) {
 	}
 
 	consumerArgs := &consumeParams{
-		QueueName:     "testing_queue",
+		QueueName:     queue.Name,
 		Consumer:      "testing_consumer",
 		ShouldAutoAck: true,
 		IsExclusive:   true,
@@ -81,5 +93,36 @@ func SendRequest(config util.Config, message string) {
 	)
 	if err != nil {
 		log.Panic("[panic] unable to declare a rabbitmq consumer", err)
+	}
+
+	corrId := uuid.New().String()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	msg := amqp.Publishing{
+		ContentType:   "text/plain",
+		CorrelationId: corrId,
+		Body:          data,
+	}
+
+	publishArgs := &publishParams{
+		Context:      ctx,
+		ExchangeName: "",
+		RoutingKey:   queue.Name,
+		IsMandatory:  false,
+		IsImmediate:  false,
+		Message:      msg,
+	}
+	err = ch.PublishWithContext(
+		publishArgs.Context,
+		publishArgs.ExchangeName,
+		publishArgs.RoutingKey,
+		publishArgs.IsMandatory,
+		publishArgs.IsImmediate,
+		publishArgs.Message,
+	)
+	if err != nil {
+		log.Panic("[panic] unable to publish message", err)
 	}
 }

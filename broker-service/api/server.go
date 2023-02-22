5ba6208854d0adb75bdcd2707f00e77333c8b095
gitSuki <collinbrown226@gmail.com>
2023-02-22 00:41:31 -0600

    Implement logic for launching gRPC server

--- broker-service/api/server.go
+++ broker-service/api/server.go
@@ -1,11 +1,17 @@
 package api
 
-import "github.com/gitsuki/finance/broker/util"
+import (
+	"github.com/gitsuki/finance/broker/pb"
+	"github.com/gitsuki/finance/broker/util"
+)
 
+// Server serves gRPC requests for our broker service
 type Server struct {
+	pb.UnimplementedBrokerServer
 	config util.Config
 }
 
+// NewServer creates a new gRPC server.
 func NewServer(config util.Config) (*Server, error) {
 	newServer := &Server{
 		config: config,
--- broker-service/main.go
+++ broker-service/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	"net"
 
 	"github.com/gitsuki/finance/broker/api"
+	"github.com/gitsuki/finance/broker/pb"
 	"github.com/gitsuki/finance/broker/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -16,5 +20,19 @@ func main() {
 	server, err := api.NewServer(config)
 	if err != nil {
 		log.Fatal("[fatal] cannot create server", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterBrokerServer(grpcServer, server)
+	reflection.Register(grpcServer) // allows gRPC client to explore which RPCs are available on the server
+
+	listener, err := net.Listen("tcp", config.ServerAddress)
+	if err != nil {
+		log.Fatal("[fatal] cannot create listener", err)
+	}
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("[fatal] unable to launch gRPC server", err)
 	}
 }
